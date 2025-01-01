c630dd2a6571f1c3bc0e13aaaeaac275b2c817ef
mykytaa <50428395+mykytaa@users.noreply.github.com>
2025-01-02 00:39:55 +0200

    Bot.py

--- Bot.py
+++ Bot.py
@@ -1,24 +1,22 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-# Замените на токен вашего бота
+# Токен вашего бота
 TOKEN = '6231361993:AAFCKT2rPnoJv2K4OXAZCpOq8KcjmFGvJjw'
 
-def start(update: Update, context: CallbackContext):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
         [InlineKeyboardButton("Открыть мини-приложение", web_app=WebAppInfo(url="https://mykytaa.github.io/Alchemy/"))]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text("Нажмите кнопку ниже, чтобы открыть мини-приложение:", reply_markup=reply_markup)
+    await update.message.reply_text("Нажмите кнопку ниже, чтобы открыть мини-приложение:", reply_markup=reply_markup)
 
 def main():
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
+    app = Application.builder().token(TOKEN).build()
 
-    dp.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("start", start))
 
-    updater.start_polling()
-    updater.idle()
+    app.run_polling()
 
 if __name__ == '__main__':
     main()
